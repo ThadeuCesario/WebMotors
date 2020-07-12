a949b437a812feb589fd09ea35fd31b0541a38d7
Thadeu Cesário <thadeu.cesario@icloud.com>
2020-07-12 15:33:13 -0300

    Form

--- webmotors/src/Components/Form/Form.jsx
+++ webmotors/src/Components/Form/Form.jsx
@@ -4,73 +4,42 @@ import api from "../../services/api";
 import { FormSearch } from "./styles";
 
 const Form = (props) => {
-  const [make, setMake] = useState(null);
-  const [makeId, setMakeId] = useState(null);
-  const [model, setModel] = useState(null);
-  const [modelId, setModelId] = useState(null);
-  const [version, setVersion] = useState(null);
+  const [vehicles, setVehicles] = useState(null);
 
   useEffect(() => {
-    api.get("Make").then((response) => {
-      setMake(response.data);
+    api.get("Vehicles?Page=5").then((response) => {
+      console.log(response.data);
     });
   }, []);
 
-  useEffect(() => {
-    if (make) {
-      let makeId = [];
-      for (let i = 0; i < make.length; i++) {
-        makeId.push(make[i]["ID"]);
-      }
-      setMakeId(makeId);
-    }
-  }, [make]);
-
-  useEffect(() => {
-    function getModel() {
-      if (makeId) {
-        for (let i = 0; i < makeId.length; i++) {
-          api.get(`Model?MakeID=${makeId[i]}`).then((response) => {
-            setModel(response.data);
-          });
-        }
-      }
-    }
-    getModel();
-  }, [makeId]);
-
-  useEffect(() => {
-    if (model) {
-      let modelId = [];
-
-      for (let i = 0; i < model.length; i++) {
-        modelId.push(model[i]["ID"]);
-      }
-      setModelId(modelId);
-    }
-  }, [model]);
-
-  useEffect(() => {
-    async function getVersion() {
-      if (modelId) {
-        for (let i = 0; i < modelId.length; i++) {
-          await api.get(`Version?ModelID=${modelId[i]}`).then((response) => {
-            console.log(response.data, i);
-          });
-        }
-      }
-    }
-    getVersion();
-  }, [modelId]);
-
-  //console.log("make", make);
-  //console.log("makeId", makeId);
-  //console.log("model", model);
-  //console.log("modelId", modelId);
-
   return (
     <FormSearch>
-      <div>AQUI ESTARÁ O FORMULARIO</div>
+      <div className="form-search">
+        <div className="form-search__option">
+          <input
+            className="form-search__option-input"
+            type="checkbox"
+            id="newCars"
+            name="newCars"
+            defaultChecked
+          />
+          <label className="form-search__option-label" htmlFor="newCars">
+            Novos
+          </label>
+        </div>
+        <div className="form-search__option">
+          <input
+            className="form-search__option-input"
+            type="checkbox"
+            id="newCars"
+            name="newCars"
+            defaultChecked
+          />
+          <label className="form-search__option-label" htmlFor="newCars">
+            Usados
+          </label>
+        </div>
+      </div>
     </FormSearch>
   );
 };
